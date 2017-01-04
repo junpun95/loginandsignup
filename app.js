06028511bdd4806c83bea1038169e8a6d852caa2
junpun95 <junpun_95@hotmail.com>
2017-01-04 11:50:00 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-/*
-
-  There are some minor modifications to the default Express setup
-  Each is commented and marked with [SH] to make them easy to find
-
- */
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -14,13 +7,13 @@ var bodyParser = require('body-parser');
 // [SH] Require Passport
 var passport = require('passport');
 
-// [SH] Bring in the data model
+// Bring in the data model
 require('./app_api/models/db');
-// [SH] Bring in the Passport config after model is defined
+// Bring in the Passport config after model is defined
 require('./app_api/config/passport');
 
 
-// [SH] Bring in the routes for the API (delete the default routes)
+// Bring in the routes for the API (delete the default routes)
 var routesApi = require('./app_api/routes/index');
 
 var app = express();
@@ -29,24 +22,22 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// [SH] Set the app_client folder to serve static resources
+// Set the app_client folder to serve static resources
 app.use(express.static(path.join(__dirname, 'app_client')));
 
-// [SH] Initialise Passport before using the route middleware
+// Initialise Passport before using the route middleware
 app.use(passport.initialize());
 
-// [SH] Use the API routes when path starts with /api
+// Use the API routes when path starts with /api
 app.use('/api', routesApi);
 
-// [SH] Otherwise render the index.html page for the Angular SPA
-// [SH] This means we don't have to map all of the SPA routes in Express
+// Otherwise render the index.html page for the Angular SPA
+// This means we don't have to map all of the SPA routes in Express
 app.use(function(req, res) {
   res.sendFile(path.join(__dirname, 'app_client', 'index.html'));
 });
@@ -61,7 +52,7 @@ app.use(function(req, res, next) {
 
 // error handlers
 
-// [SH] Catch unauthorised errors
+// Catch unauthorised errors
 app.use(function (err, req, res, next) {
   if (err.name === 'UnauthorizedError') {
     res.status(401);
